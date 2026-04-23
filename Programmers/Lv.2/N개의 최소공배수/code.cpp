7adedab2e07582e93603772383cef97101037342
Minki Cho <130172235+HermitCho@users.noreply.github.com>
2026-04-23 11:44:52 +0900

    프로그래머스 Lv.2 'N개의 최소공배수' 문제 풀이 완료.

---
+++ Programmers/Lv.2/N개의 최소공배수/code.cpp
@@ -0,0 +1,33 @@
+﻿#include <string>
+#include <vector>
+
+using namespace std;
+
+int gcd(int a, int b)
+{
+    int tempN;
+    while (b > 0)
+    {
+        tempN = a % b;
+        a = b;
+        b = tempN;
+    }
+    return a;
+}
+
+int lcm(int a, int b)
+{
+    if (a <= 0 && b <= 0) return 0;
+    return (a / gcd(a, b)) * b;
+}
+
+int solution(vector<int> arr) {
+    int answer = arr[0];
+
+    for (int i = 1; i < arr.size(); ++i)
+    {
+        answer = lcm(answer, arr[i]);
+    }
+
+    return answer;
+}
