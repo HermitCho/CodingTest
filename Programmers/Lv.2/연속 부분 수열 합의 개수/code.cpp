e653162d17da50c3392b92eda5d4b3e98203ea36
Minki Cho <130172235+HermitCho@users.noreply.github.com>
2026-05-04 10:45:49 +0900

    프로그래머스 Lv.2 '연속 부분 수열 합의 개수' 문제 풀이 완료.

---
+++ Programmers/Lv.2/연속 부분 수열 합의 개수/code.cpp
@@ -0,0 +1,26 @@
+﻿#include <string>
+#include <vector>
+#include <set>
+
+using namespace std;
+
+int solution(vector<int> elements) {
+    int answer;
+    set<int> distinctSums;
+    int n = elements.size();
+
+    for (int i = 0; i < n; ++i) {
+        int currentSum = 0;
+
+        for (int len = 1; len <= n; ++len)
+        {
+            int currentIndex = (i + len - 1) % n;
+            currentSum += elements[currentIndex];
+
+            distinctSums.insert(currentSum);
+        }
+    }
+
+    answer = distinctSums.size();
+    return answer;
+}
