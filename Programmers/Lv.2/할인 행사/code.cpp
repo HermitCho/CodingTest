1d950160ec88596ac7ca54c947b02fb78329f73d
Minki Cho <130172235+HermitCho@users.noreply.github.com>
2026-05-12 10:18:46 +0900

    프로그래머스 Lv.2 '할인 행사' 문제 풀이 완료.

---
+++ Programmers/Lv.2/할인 행사/code.cpp
@@ -0,0 +1,39 @@
+﻿#include <string>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+bool checkMatch(unordered_map<string, int>& wantMap, unordered_map<string, int>& discountMap) {
+    for (auto const& [item, count] : wantMap) {
+        if (discountMap[item] < count) return false;
+    }
+    return true;
+}
+
+int solution(vector<string> want, vector<int> number, vector<string> discount) {
+    int answer = 0;
+    unordered_map<string, int> wantMap;
+
+    for (int i = 0; i < want.size(); ++i) {
+        wantMap[want[i]] = number[i];
+    }
+
+    unordered_map<string, int> discountMap;
+    for (int i = 0; i < 10; ++i) {
+        discountMap[discount[i]]++;
+    }
+
+    if (checkMatch(wantMap, discountMap)) answer++;
+
+    for (int i = 10; i < discount.size(); ++i) {
+        discountMap[discount[i]]++;
+        discountMap[discount[i - 10]]--;
+
+        if (checkMatch(wantMap, discountMap)) {
+            answer++;
+        }
+    }
+
+    return answer;
+}
