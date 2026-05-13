59d5605fdda9f5e99d19ef07e06ba9a394aa3c2c
Minki Cho <130172235+HermitCho@users.noreply.github.com>
2026-05-13 10:11:44 +0900

    프로그래머스 Lv.2 '의상' 문제 풀이 완료.

---
+++ Programmers/Lv.2/의상/code.cpp
@@ -0,0 +1,21 @@
+﻿#include <string>
+#include <vector>
+#include <map>
+using namespace std;
+
+int solution(vector<vector<string>> clothes) {
+    int answer = 1;
+    map<string, int> clothType;
+
+    for (int i = 0; i < clothes.size(); ++i)
+    {
+        ++clothType[clothes[i][1]];
+    }
+
+    for (auto const& it : clothType)
+    {
+        answer *= (it.second + 1);
+    }
+
+    return answer - 1;
+}
