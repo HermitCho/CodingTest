435a42c906de3dd3301d9a74ef044c9c5b2cd352
Minki Cho <130172235+HermitCho@users.noreply.github.com>
2026-05-19 15:33:33 +0900

    프로그래머스 Lv.2 '피로도' 문제 풀이 완료.

---
+++ Programmers/Lv.2/피로도/code.cpp
@@ -0,0 +1,35 @@
+﻿#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int maxDungeons = 0;
+vector<bool> visited;
+
+void backtracking(int currentFatigue, int dunCount, const vector<vector<int>>& dungeons) {
+
+    maxDungeons = max(maxDungeons, dunCount);
+
+    for (int i = 0; i < dungeons.size(); ++i) {
+
+        if (!visited[i] && currentFatigue >= dungeons[i][0]) {
+
+            visited[i] = true;
+
+            backtracking(currentFatigue - dungeons[i][1], dunCount + 1, dungeons);
+
+            visited[i] = false;
+        }
+    }
+}
+
+int solution(int k, vector<vector<int>> dungeons) {
+
+    visited.assign(dungeons.size(), false);
+    maxDungeons = 0;
+
+    backtracking(k, 0, dungeons);
+
+    return maxDungeons;
+}
