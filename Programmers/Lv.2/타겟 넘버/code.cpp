3f1abf9e09f1ce5fceb530820915c643715cfb67
Minki Cho <130172235+HermitCho@users.noreply.github.com>
2026-05-20 09:56:17 +0900

    프로그래머스 Lv.2 '타겟 넘버' 문제 풀이 완료.

---
+++ Programmers/Lv.2/타겟 넘버/code.cpp
@@ -0,0 +1,26 @@
+﻿#include <string>
+#include <vector>
+
+using namespace std;
+
+int backtracking(int target, int elementLoc, int numSum, vector<int>& numbers)
+{
+    if (elementLoc == numbers.size())
+    {
+        return (target == numSum) ? 1 : 0;
+    }
+
+    int plus_case = backtracking(target, elementLoc + 1, numSum + numbers[elementLoc], numbers);
+    int minus_case = backtracking(target, elementLoc + 1, numSum - numbers[elementLoc], numbers);
+
+    return plus_case + minus_case;
+}
+
+int solution(vector<int> numbers, int target)
+{
+    int answer = 0;
+
+    answer = backtracking(target, 0, 0, numbers);
+
+    return answer;
+}
