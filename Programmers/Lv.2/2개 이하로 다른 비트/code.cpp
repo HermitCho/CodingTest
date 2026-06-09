5e49b2dd2008574decc23664edf025ea1d5efc88
Minki Cho <130172235+HermitCho@users.noreply.github.com>
2026-06-09 11:03:22 +0900

    프로그래머스 Lv.2 '2개 이하로 다른 비트' 문제 풀이 완료.

---
+++ Programmers/Lv.2/2개 이하로 다른 비트/code.cpp
@@ -0,0 +1,22 @@
+﻿#include <vector>
+
+using namespace std;
+
+vector<long long> solution(vector<long long> numbers) {
+    vector<long long> answer;
+    answer.reserve(numbers.size());
+
+    for (long long x : numbers) {
+        if (x % 2 == 0)
+        {
+            answer.push_back(x + 1);
+        }
+        else
+        {
+            long long lowest_zero = (x + 1) & -(x + 1);
+            answer.push_back(x + (lowest_zero >> 1));
+        }
+    }
+
+    return answer;
+}
