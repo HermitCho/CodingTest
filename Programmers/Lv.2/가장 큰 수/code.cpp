e2ee0444c049959f8f4dab4d7da417d294f134dc
Minki Cho <130172235+HermitCho@users.noreply.github.com>
2026-06-11 09:49:33 +0900

    프로그래머스 Lv.2 '가장 큰 수' 문제 풀이 완료.

---
+++ Programmers/Lv.2/가장 큰 수/code.cpp
@@ -0,0 +1,29 @@
+﻿#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+string solution(vector<int> numbers) {
+    string answer = "";
+
+
+    sort(numbers.begin(), numbers.end(), [](int a, int b)
+        {
+            string string_a = to_string(a);
+            string string_b = to_string(b);
+            return string_a + string_b > string_b + string_a;
+        });
+
+    for (int i = 0; i < numbers.size(); i++)
+    {
+        answer += (to_string(numbers[i]));
+    }
+
+    if (answer[0] == '0')
+    {
+        answer = "0";
+    }
+
+    return answer;
+}
