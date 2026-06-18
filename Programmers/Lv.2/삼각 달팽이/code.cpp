1b63050d94a03eef65f0b8845888e730d0d3b4ee
Minki Cho <130172235+HermitCho@users.noreply.github.com>
2026-06-18 12:34:12 +0900

    프로그래머스 Lv.2 '삼각 달팽이' 문제 풀이 완료.

---
+++ Programmers/Lv.2/삼각 달팽이/code.cpp
@@ -0,0 +1,42 @@
+﻿#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(int n) {
+    vector<int> answer;
+
+    vector<vector<int>> triangle(n);
+    for (int i = 0; i < n; ++i) {
+        triangle[i].resize(i + 1);
+    }
+
+    int row = -1;
+    int col = 0;
+    int num = 1;
+
+    for (int i = 0; i < n; ++i) {
+        for (int j = i; j < n; ++j) {
+            if (i % 3 == 0) {
+                row++;
+            }
+            else if (i % 3 == 1) {
+                col++;
+            }
+            else if (i % 3 == 2) {
+                row--;
+                col--;
+            }
+
+            triangle[row][col] = num++;
+        }
+    }
+
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j <= i; ++j) {
+            answer.push_back(triangle[i][j]);
+        }
+    }
+
+    return answer;
+}
