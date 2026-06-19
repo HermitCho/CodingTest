f608e6ef71b2c57be0a0683d10f05893dbd7ad24
Minki Cho <130172235+HermitCho@users.noreply.github.com>
2026-06-19 20:12:40 +0900

    프로그래머스 Lv.2 '연속된 부분 수열의 합' 문제 풀이 완료.

---
+++ Programmers/Lv.2/연속된 부분 수열의 합/code.cpp
@@ -0,0 +1,38 @@
+﻿#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> sequence, int k) {
+    int left = 0;
+    int right = 0;
+    int sum = sequence[0];
+
+    int min_len = 2000000;
+    vector<int> answer(2, 0);
+
+    while (right < sequence.size()) {
+
+        if (sum == k) {
+
+            if (right - left < min_len) {
+                min_len = right - left;
+                answer[0] = left;
+                answer[1] = right;
+            }
+        }
+
+        if (sum <= k) {
+            right++;
+            if (right < sequence.size()) {
+                sum += sequence[right];
+            }
+        }
+
+        else {
+            sum -= sequence[left];
+            left++;
+        }
+    }
+
+    return answer;
+}
