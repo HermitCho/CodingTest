cbf2a14eae9cb7ae3d0412a0a7ca5713bc134881
Minki Cho <130172235+HermitCho@users.noreply.github.com>
2026-06-22 10:53:15 +0900

    프로그래머스 Lv.2 '두 큐 합 같게 만들기' 문제 풀이 완료.

---
+++ Programmers/Lv.2/두 큐 합 같게 만들기/code.cpp
@@ -0,0 +1,51 @@
+﻿#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> queue1, vector<int> queue2) {
+    long long sum1 = 0;
+    long long sum2 = 0;
+
+    for (int num : queue1) sum1 += num;
+    for (int num : queue2) sum2 += num;
+
+    long long totalSum = sum1 + sum2;
+
+    if (totalSum % 2 != 0) {
+        return -1;
+    }
+
+    long long target = totalSum / 2;
+    int n = queue1.size();
+
+    vector<int> q;
+    q.reserve(n * 2);
+    for (int num : queue1) q.push_back(num);
+    for (int num : queue2) q.push_back(num);
+
+    int p1 = 0;
+    int p2 = n;
+    int answer = 0;
+
+    int max_ops = n * 4;
+
+    while (answer <= max_ops) {
+        if (sum1 == target) {
+            return answer;
+        }
+
+        if (sum1 > target) {
+            sum1 -= q[p1 % (2 * n)];
+            p1++;
+        }
+        else {
+            sum1 += q[p2 % (2 * n)];
+            p2++;
+        }
+
+        answer++;
+    }
+
+    return -1;
+}
